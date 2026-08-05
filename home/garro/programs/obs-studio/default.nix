98aeb4a3937c88961e557e8ea1880394fd86a986
Santiago-Garrote <santiagogarrote2005@gmail.com>
2026-08-04 22:44:44 -0300

    fix(obs): force managed config files

--- home/garro/programs/obs-studio/default.nix
+++ home/garro/programs/obs-studio/default.nix
@@ -20,34 +20,40 @@
     wf-recorder
   ];
 
-  xdg.configFile."obs-studio/global.ini".text = ''
-    [General]
-    CurrentProfile=Screen Recording
-    CurrentSceneCollection=Firefox Capture
+  xdg.configFile."obs-studio/global.ini" = {
+    force = true;
+    text = ''
+      [General]
+      CurrentProfile=Screen Recording
+      CurrentSceneCollection=Firefox Capture
 
-    [Basic]
-    ProfileDir=Screen Recording
-    SceneCollection=Firefox Capture
-    SceneCollectionFile=Firefox Capture
-  '';
+      [Basic]
+      ProfileDir=Screen Recording
+      SceneCollection=Firefox Capture
+      SceneCollectionFile=Firefox Capture
+    '';
+  };
 
-  xdg.configFile."obs-studio/basic/profiles/Screen Recording/basic.ini".text = ''
-    [General]
-    Name=Screen Recording
+  xdg.configFile."obs-studio/basic/profiles/Screen Recording/basic.ini" = {
+    force = true;
+    text = ''
+      [General]
+      Name=Screen Recording
 
-    [Output]
-    Mode=Simple
+      [Output]
+      Mode=Simple
 
-    [SimpleOutput]
-    FilePath=/home/garro/Videos/Recordings
-    RecFormat2=mkv
-    RecQuality=Small
-    VBitrate=12000
-    ABitrate=320
+      [SimpleOutput]
+      FilePath=/home/garro/Videos/Recordings
+      RecFormat2=mkv
+      RecQuality=Small
+      VBitrate=12000
+      ABitrate=320
 
-    [Audio]
-    SampleRate=48000
-  '';
+      [Audio]
+      SampleRate=48000
+    '';
+  };
 
   home.activation.createObsRecordingDirectory = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD install -d $VERBOSE_ARG "${config.home.homeDirectory}/Videos/Recordings"
