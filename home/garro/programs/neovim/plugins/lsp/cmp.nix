e1939a30ef2a7c67fae645d86d1c5cbb3ac9308c
Santiago-Garrote <santiagogarrote2005@gmail.com>
2026-08-11 20:37:24 -0300

    fix(neovim.cmp): fixed Tab notation

--- home/garro/programs/neovim/plugins/lsp/cmp.nix
+++ home/garro/programs/neovim/plugins/lsp/cmp.nix
@@ -2,57 +2,57 @@
 
 {
   plugins.cmp = {
-  enable = true;
-  
-  autoEnableSources = true;
-  
-  settings = {
-  completion = {
-  completeopt = "menu,menuone,noselect";
-  };
-  
-  snippet = {
-  expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-  };
-  
-  mapping = {
-  "<C-Space>" = "cmp.mapping.complete()";
-  "<C-e>" = "cmp.mapping.abort()";
-  
-  "<CR>" = "cmp.mapping.confirm({ select = false })";
-  
-  "<Tab" = "cmp.mapping.select_next_item()";
-  "<S-Tab>" = "cmp.mapping.select_prev_item()";
-  
-  "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-  "<C-f>" = "cmp.mapping.scroll_docs(4)";
-  };
-  
-  sources = [
-  {
-  name = "nvim_lsp";
-  }
-  {
-  name = "luasnip";
-  }
-  {
-  name = "path";
-  }
-  {
-  name = "buffer";
-  }
-  ];
-  
-  window = {
-  completion = {
-  border = "rounded";
-  };
-  
-  documentation = {
-  border = "rounded";
-  };
-  };
-  };
+    enable = true;
+
+    autoEnableSources = true;
+
+    settings = {
+      completion = {
+        completeopt = "menu,menuone,noselect";
+      };
+
+      snippet = {
+        expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+      };
+
+      mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-e>" = "cmp.mapping.abort()";
+
+        "<CR>" = "cmp.mapping.confirm({ select = false })";
+
+        "<Tab>" = "cmp.mapping.select_next_item()";
+        "<S-Tab>" = "cmp.mapping.select_prev_item()";
+
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      };
+
+      sources = [
+        {
+          name = "nvim_lsp";
+        }
+        {
+          name = "luasnip";
+        }
+        {
+          name = "path";
+        }
+        {
+          name = "buffer";
+        }
+      ];
+
+      window = {
+        completion = {
+          border = "rounded";
+        };
+
+        documentation = {
+          border = "rounded";
+        };
+      };
+    };
   };
 
 }
