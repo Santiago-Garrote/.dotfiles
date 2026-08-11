c261627aceb9cfdbf4ee5bbb57ba68e106028cb7
Santiago-Garrote <santiagogarrote2005@gmail.com>
2026-08-11 17:40:35 -0300

    feature(nvim.lsp): i added support for java and groovy lsps

--- home/garro/programs/neovim/plugins/lsp.nix
+++ home/garro/programs/neovim/plugins/lsp.nix
@@ -3,7 +3,23 @@
 {
   plugins.cmp.enable = true;
   plugins.cmp-nvim-lsp.enable = true;
-  plugins.lsp.enable = true;
+  plugins.lsp = {
+    enable = true;
+
+    servers = {
+        jdtls = {
+          enable = true;
+          packageFallback = true;
+	  package = null;
+        };
+
+        groovyls = {
+          enable = true;
+          packageFallback = true;
+	  package = null;
+        };
+    };
+  };
   plugins.luasnip.enable = true;
   plugins.none-ls.enable = true;
 }
